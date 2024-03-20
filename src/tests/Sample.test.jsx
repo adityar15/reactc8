1cd37dc2d66825deec1ca2e62cdc006add02c0e8
Aditya Kadam <adityakadam@Adityas-MacBook-Air-2.local>
2024-03-20 17:42:52 +0000

    tests added

---
+++ src/tests/Sample.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Heading from '../components/Heading'
+
+import { expect, test } from 'vitest'
+
+// tools used
+// react-test-renderer happy-dom jsdom 
+
+// function toJson(component) {
+//   const result = component.toJSON()
+//   expect(result).toBeDefined()
+//   expect(result).not.toBeInstanceOf(Array)
+//   return result
+// }
+test('Heading component', () => {
+
+  const component = renderer.create(<Heading title="Adi" />);
+  let tree = component.toJSON()
+  console.log("to json", tree)
+  expect(tree).toMatchSnapshot();
+
+
+  // If you need to test different states or props, you can add more test cases here.
+});
