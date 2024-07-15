b5810abff6e1c64da814043fa8df07a5ed44e6d6
Aditya Kadam <adityakadam@Adityas-MacBook-Air-2.local>
2024-07-15 19:00:48 +0100

    react s3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { createContext } from 'react'
+import ProductCard from './components/ProductCard'
+import Header from './components/Header'
+import CartProvider from './components/CartProvider'
 
 
-import GithubSearch from './components/GithubSearch'
+// import GithubSearch from './components/GithubSearch'
+
+
 
 
 export default function App() {
   return (
-    <div className='max-w-md mx-auto p-2'>
-      <GithubSearch />
-    </div>
+    <CartProvider>
+      <Header />
+      <div className='max-w-4xl mx-auto p-2'>
+        {/* <GithubSearch /> */}
+        <ProductCard />
+      </div>
+    </CartProvider>
   )
 }
